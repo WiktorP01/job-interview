d51f5d775a69799d389cabcf3cce48c06a1ef79f
wp01 <wiktorpluta01@gmail.com>
2025-02-25 20:56:22 +0100

    Initial commit

---
+++ src/main/java/com/hrs/reservation_task/ReservationTaskApplication.java
@@ -0,0 +1,13 @@
+package com.hrs.reservation_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrs/reservation_task/ReservationTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrs.reservation_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
